1c0aa3789e33835e59cc44dea3a51db2af6c0ba8
dvidCR <dvidcr100@gmail.com>
2025-02-18 13:00:05 +0100

    Prueba Workflow3

---
+++ GestorDeReportes/src/test/java/testDatabase/TestQuery.java
@@ -0,0 +1,25 @@
+package testDatabase;
+
+public class TestQuery {
+//	public static void main(String[] args) {
+//  Employes empleados = mock(Employes.class);
+//}
+//
+//@Test
+//public void testCalcularSuma() {
+//  
+//
+//  // 2. Definir el comportamiento del mock
+//  when(servicioMock.suma(5, 3)).thenReturn(10);
+//
+//  // 3. Usar el mock en la clase que queremos probar
+//  Calculadora calculadora = new Calculadora(servicioMock);
+//
+//  // 4. Llamar al método y verificar el resultado
+//  int resultado = calculadora.calcularSuma(5, 3);
+//  assertEquals(10, resultado);
+//
+//  // 5. Verificar que el método fue llamado correctamente
+//  verify(servicioMock).suma(5, 3);
+//}
+}
